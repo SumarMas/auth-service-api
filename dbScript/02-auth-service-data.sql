a66e2af59abca4fdc72dcd745853316c6f798290
EbeltramoUtn <166845082+EbeltramoUtn@users.noreply.github.com>
2025-11-16 17:58:07 -0300

    Feature/sumar retry eureka (#4)
    * Update config and SQL for new environment settings
    
    Changed database password, port, and service URLs in application.yml to match new environment. Updated BCrypt hash for admin password in SQL script.
    
    * Update base image and tune Eureka client settings
    
    Switched Docker base image to eclipse-temurin:17-jre-alpine for a lighter runtime. Adjusted Eureka client configuration to reduce registry fetch and lease intervals for faster service registration and discovery.

--- dbScript/02-auth-service-data.sql
+++ dbScript/02-auth-service-data.sql
@@ -11,7 +11,7 @@ INSERT INTO user_credentials (
              UUID_TO_BIN('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 1), -- nuevo UUID para auth-service
              UUID_TO_BIN('11111111-1111-1111-1111-111111111111', 1), -- user_id ya existente en user-service
              'admin',
-             '$2a$10$evdIFDkt6MdAhLx4tpErTeUSe7XDCiis2ARWZ6l0B0JP3C/mU9VQm', -- BCrypt de 'admin123'
+             '$2a$10$Ln2dUxlz0NgtEwxza.PG0uFsz8zGf2D0zTouys8FQrq8n9S2U4try', -- BCrypt de 'admin123'
              1,
              UUID_TO_BIN('11111111-1111-1111-1111-111111111111', 1),
              UUID_TO_BIN('11111111-1111-1111-1111-111111111111', 1)
